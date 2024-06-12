189f7ed18ca7e63a361f5b0239e040bf49b1a569
Sandesh-BuildApp <166100480+Sandesh-BuildApp@users.noreply.github.com>
2024-06-12 14:52:26 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Update the package list and install libx11-6
-apt-get update
-apt-get install -y libx11-6
+# Update and install dependencies
+apt-get update && apt-get install -y libx11-6
+
+# Install any additional dependencies your application needs
+# For example, you might need to install OpenCV dependencies
+# apt-get install -y libopencv-dev
 
 # Start the Flask application
-python -u app.py
+# python app.py
+gunicorn --bind=0.0.0.0 --timeout 600 app:app
