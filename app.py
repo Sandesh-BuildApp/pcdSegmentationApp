8a8d6de66af2bef7b4cfe73fa83a376a9c84e381
Sandesh-BuildApp <166100480+Sandesh-BuildApp@users.noreply.github.com>
2024-06-26 10:43:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -396,7 +396,6 @@ def floorGAN():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    # app.run(debug=True)
     # app.run(host='127.0.0.1', port=8080, debug=True)
     app.run(host='0.0.0.0', port=8080, debug=True)
     # app.run(host='0.0.0.0', port=8080, threaded=True)
